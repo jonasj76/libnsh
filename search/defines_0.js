115f31cd6a82ced7531e57113af2065fb27b46ab
Jonas Johansson <jonas.johansson@westermo.se>
2016-04-06 21:04:15 +0200

    Add doxygen documentation
    Signed-off-by: Jonas Johansson <jonas.johansson@westermo.se>

---
+++ search/defines_0.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['nsh_5fregister_5fscalar_5fro',['nsh_register_scalar_ro',['../nsh_8h.html#a6afc4a9ba4df6a73b4989ff95aad066f',1,'nsh.h']]],
+  ['nsh_5fregister_5fscalar_5frw',['nsh_register_scalar_rw',['../nsh_8h.html#ae0f69efd83afd6a3ea57507a3ec87d13',1,'nsh.h']]],
+  ['nsh_5fscalar_5fgroup_5fhandler_5fro',['nsh_scalar_group_handler_ro',['../nsh_8h.html#ae67b3d10c3f2910a331b74c1aa7a0458',1,'nsh.h']]],
+  ['nsh_5fscalar_5fgroup_5fhandler_5frw',['nsh_scalar_group_handler_rw',['../nsh_8h.html#aa13854743034b98f7d9b187025f52cc8',1,'nsh.h']]],
+  ['nsh_5fscalar_5fhandler_5fint',['nsh_scalar_handler_int',['../nsh_8h.html#af04870c5584bbe471897ebb496cb6b23',1,'nsh.h']]],
+  ['nsh_5fscalar_5fhandler_5fro',['nsh_scalar_handler_ro',['../nsh_8h.html#afe8d30c7633f2d8cb5ed57dc60979c5f',1,'nsh.h']]],
+  ['nsh_5fscalar_5fhandler_5frw',['nsh_scalar_handler_rw',['../nsh_8h.html#a4cd0bef8bdd973e39f6f16a6eee5f820',1,'nsh.h']]],
+  ['nsh_5fscalar_5fstr_5fhandler_5fro',['nsh_scalar_str_handler_ro',['../nsh_8h.html#a43cf01aa7e0dc1580aca4308c95b8c78',1,'nsh.h']]],
+  ['nsh_5fscalar_5fstr_5fhandler_5frw',['nsh_scalar_str_handler_rw',['../nsh_8h.html#af298d485c310e151ece29123a5af6288',1,'nsh.h']]]
+];
---
+++ search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['nsh_2eh',['nsh.h',['../nsh_8h.html',1,'']]]
+];
