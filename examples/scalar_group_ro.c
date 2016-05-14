d15a07a94a8920f215fbf8892985258eeff37338
Jonas Johansson <jonasj76@gmail.com>
2016-05-14 12:31:09 +0200

    examples: update file headers
    Signed-off-by: Jonas Johansson <jonasj76@gmail.com>

--- examples/scalar_group_ro.c
+++ examples/scalar_group_ro.c
@@ -1,8 +1,24 @@
-/* libnsh example -- Groups. Use one callback for several OIDs.
+/* libnsh example - Use one callback for several OIDs
  *
- * This example will register two OIDs, 1.3.6.1.4.1.8072.9999.9999.1
- * and 1.3.6.1.4.1.8072.9999.9999.2.
- * .1 will return an integer value, and .2 will return a string.
+ * Copyright (c) 2016  Jonas Johansson <jonasj76@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 #include <stdlib.h>
